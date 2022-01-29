72f34e4b1196417f895644d8f3bda60aa679eac7
Grzegorz Dudek <Greketrotny@Gmail.com>
2022-01-29 21:57:55 +0100

    separate Renderer

---
+++ RayZath/cuda_engine_renderer.cu
@@ -0,0 +1,373 @@
+#include "cuda_engine_renderer.cuh"
+
+#include "cuda_engine_core.cuh"
+
+#include "cuda_preprocess_kernel.cuh"
+#include "cuda_render_kernel.cuh"
+#include "cuda_postprocess_kernel.cuh"
+
+#include <algorithm>
+#include <ios>
+#include <sstream>
+
+namespace RayZath::Cuda
+{
+	// ~~~~~~~~ [STRUCT] Indexer ~~~~~~~~
+	Indexer::Indexer()
+		: m_update_idx(0u)
+		, m_render_idx(1u)
+	{}
+
+	const bool& Indexer::UpdateIdx() const
+	{
+		return m_update_idx;
+	}
+	const bool& Indexer::RenderIdx() const
+	{
+		return m_render_idx;
+	}
+	void Indexer::Swap()
+	{
+		std::swap(m_update_idx, m_render_idx);
+	}
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+	// ~~~~~~~~ [STRUCT] TimeTable ~~~~~~~~
+	TimeTable::TimeTable()
+	{
+		m_timer.Start();
+	}
+
+	void TimeTable::AppendStage(const std::string& s)
+	{
+		m_stamps.push_back({ s, m_timer.GetTime() });
+	}
+	void TimeTable::AppendFullCycle(const std::string& s)
+	{
+		m_stamps.push_back({ s, m_cycle_timer.GetTime() });
+	}
+	void TimeTable::ResetTime()
+	{
+		m_timer.Start();
+		m_cycle_timer.Start();
+	}
+	void TimeTable::ResetTable()
+	{
+		m_stamps.clear();
+	}
+	std::string TimeTable::ToString(const uint32_t width) const
+	{
+		std::stringstream ss;
+		for (auto& stamp : m_stamps)
+		{
+			ss.fill(' ');
+			ss.width(width);
+			ss << stamp.first << ": ";
+
+			std::ignore = ss.width();
+			ss.precision(3);
+			ss << std::fixed << stamp.second << "ms\n";
+		}
+		return ss.str();
+	}
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+	Renderer::Renderer(EngineCore* const engine_core)
+		: mp_engine_core(engine_core)
+		, m_is_thread_alive(false)
+		, m_terminate_thread(false)
+		, m_state(State::None)
+		, m_stage(Stage::None)
+		, mp_blocking_gate(nullptr)
+		, m_fence_track(true)
+	{}
+	Renderer::~Renderer()
+	{
+		TerminateThread();
+	}
+
+	void Renderer::LaunchThread()
+	{
+		std::lock_guard<std::mutex> lg(m_mtx);
+		if (!m_is_thread_alive)
+		{
+			m_terminate_thread = false;
+			m_is_thread_alive = true;
+			mp_blocking_gate = nullptr;
+			ResetExceptions();
+
+			if (mp_render_thread)
+			{
+				if (mp_render_thread->joinable())
+					mp_render_thread->join();
+			}
+			mp_render_thread.reset(new std::thread(
+				&Renderer::RenderFunctionWrapper,
+				this));
+		}
+	}
+	void Renderer::TerminateThread()
+	{
+		{
+			std::lock_guard<std::mutex> lg(m_mtx);
+			m_terminate_thread = true;
+		}
+		if (m_is_thread_alive)
+		{
+			mp_engine_core->GetFenceTrack().OpenAll();
+
+			if (mp_render_thread->joinable())
+				mp_render_thread->join();
+
+			mp_render_thread.reset();
+			m_is_thread_alive = false;
+		}
+	}
+
+	FenceTrack<5>& Renderer::GetFenceTrack()
+	{
+		return m_fence_track;
+	}
+	const TimeTable& Renderer::GetTimeTable() const
+	{
+		return m_time_table;
+	}
+	const Renderer::State& Renderer::GetState() const
+	{
+		return m_state;
+	}
+	const Renderer::Stage& Renderer::GetStage() const
+	{
+		return m_stage;
+	}
+
+	void Renderer::SetState(const Renderer::State& state)
+	{
+		m_state = state;
+	}
+	void Renderer::SetStage(const Renderer::Stage& stage)
+	{
+		m_stage = stage;
+	}
+
+	void Renderer::RenderFunctionWrapper()
+	{
+		RenderFunction();
+		std::lock_guard<std::mutex> lg(m_mtx);
+		m_is_thread_alive = false;
+		m_state = State::None;
+		m_stage = Stage::None;
+		m_fence_track.OpenAll();
+	}
+	void Renderer::RenderFunction() noexcept
+	{
+		m_time_table.ResetTime();
+		try
+		{
+			while (!m_terminate_thread)
+			{
+				SetState(State::Idle);
+				SetStage(Stage::Idle);
+
+				// fine idling
+
+				SetState(State::None);
+				SetStage(Stage::None);
+				m_fence_track.OpenGate(size_t(Renderer::Stage::Idle));
+				mp_engine_core->GetFenceTrack().WaitForEndOfAndClose(size_t(EngineCore::Stage::Synchronization));
+				if (CheckTermination()) return;
+				m_time_table.ResetTable();
+				m_time_table.AppendStage("wait for host");
+
+
+				// Preprocess
+				SetState(State::Work);
+				SetStage(Stage::Preprocess);
+				const auto& configs = mp_engine_core->GetLaunchConfigs(
+					mp_engine_core->GetIndexer().RenderIdx()).GetConfigs();
+				for (const auto& config : configs)
+				{
+					cudaSetDevice(config.GetDeviceId());
+
+					if (config.GetUpdateFlag())
+					{
+						Kernel::DepthBufferReset
+							<< <
+							config.GetGrid(),
+							config.GetThreadBlock(),
+							0u,
+							mp_engine_core->GetRenderStream()
+							>> >
+							(mp_engine_core->GetCudaWorld(), config.GetCameraId());
+						CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+						CudaErrorCheck(cudaGetLastError());
+					}
+					m_time_table.AppendStage("buffer reset");
+
+					Kernel::CudaCameraUpdateSamplesNumber
+						<< <
+						1u, 1u, 0u, mp_engine_core->GetRenderStream()
+						>> >
+						(mp_engine_core->GetCudaWorld(),
+							config.GetCameraId(),
+							config.GetUpdateFlag());
+					CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+					CudaErrorCheck(cudaGetLastError());
+					m_time_table.AppendStage("sample update");
+				}
+
+				SetState(State::Wait);
+				SetStage(Stage::None);
+				m_fence_track.OpenGate(size_t(Stage::Preprocess));
+				if (CheckTermination()) return;
+				//mp_engine_core->GetFenceTrack().WaitForEndOfAndClose(?)
+
+
+				// Main render
+				SetState(State::Work);
+				SetStage(Stage::MainRender);
+				for (const auto& config : configs)
+				{
+					if (config.GetUpdateFlag())
+					{
+						Kernel::LaunchFirstPass
+							<< <
+							config.GetGrid(),
+							config.GetThreadBlock(),
+							config.GetSharedMemorySize(),
+							mp_engine_core->GetRenderStream()
+							>> >
+							(mp_engine_core->GetGlobalKernel(mp_engine_core->GetIndexer().RenderIdx()),
+								mp_engine_core->GetCudaWorld(),
+								config.GetCameraId());
+						CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+						CudaErrorCheck(cudaGetLastError());
+						m_time_table.AppendStage("main render");
+
+
+						Kernel::SpacialReprojection
+							<< <
+							config.GetGrid(),
+							config.GetThreadBlock(),
+							0u,
+							mp_engine_core->GetRenderStream()
+							>> >
+							(mp_engine_core->GetCudaWorld(), config.GetCameraId());
+						CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+						CudaErrorCheck(cudaGetLastError());
+						m_time_table.AppendStage("reprojection");
+					}
+					else
+					{
+						Kernel::LaunchCumulativePass
+							<< <
+							config.GetGrid(),
+							config.GetThreadBlock(),
+							config.GetSharedMemorySize(),
+							mp_engine_core->GetRenderStream()
+							>> >
+							(mp_engine_core->GetGlobalKernel(mp_engine_core->GetIndexer().RenderIdx()),
+								mp_engine_core->GetCudaWorld(),
+								config.GetCameraId());
+						CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+						CudaErrorCheck(cudaGetLastError());
+						m_time_table.AppendStage("main render");
+						m_time_table.AppendStage("reprojection");
+					}
+				}
+
+				SetState(State::Wait);
+				SetStage(Stage::None);
+				m_fence_track.OpenGate(size_t(Stage::MainRender));
+				mp_engine_core->GetFenceTrack().WaitForEndOfAndClose(size_t(EngineCore::Stage::ResultTransfer));
+				if (CheckTermination()) return;
+				m_time_table.AppendStage("wait for result transfer");
+
+				// Postprocess
+				SetState(State::Work);
+				SetStage(Stage::Postprocess);
+				for (const auto& config : configs)
+				{
+					Kernel::ToneMap
+						<< <
+						config.GetGrid(),
+						config.GetThreadBlock(),
+						0u,
+						mp_engine_core->GetRenderStream()
+						>> >
+						(mp_engine_core->GetGlobalKernel(mp_engine_core->GetIndexer().RenderIdx()),
+							mp_engine_core->GetCudaWorld(),
+							config.GetCameraId());
+					CudaErrorCheck(cudaStreamSynchronize(mp_engine_core->GetRenderStream()));
+					CudaErrorCheck(cudaGetLastError());
+
+					m_time_table.AppendStage("tone mapping");
+				}
+
+				m_time_table.AppendFullCycle("full render cycle");
+
+				SetState(State::Idle);
+				SetStage(Stage::Idle);
+				m_fence_track.OpenGate(size_t(Stage::Postprocess));
+			}
+		}
+		catch (const CudaException& e)
+		{
+			ReportCudaException(e);
+		}
+		catch (const Exception& e)
+		{
+			ReportException(e);
+		}
+		catch (...)
+		{
+			ReportException(Exception(
+				"Rendering function unknown exception.",
+				__FILE__, __LINE__));
+		}
+	}
+	bool Renderer::CheckTermination()
+	{
+		return m_terminate_thread;
+	}
+
+	void Renderer::ReportException(const Exception& e)
+	{
+		if (!m_exception)
+		{
+			m_exception.reset(new Exception(e));
+		}
+	}
+	void Renderer::ReportCudaException(const CudaException& e)
+	{
+		if (!m_cuda_exception)
+		{
+			m_cuda_exception.reset(new CudaException(e));
+		}
+	}
+	void Renderer::ResetExceptions()
+	{
+		m_exception = nullptr;
+		m_cuda_exception = nullptr;
+	}
+	void Renderer::ThrowIfException()
+	{
+		if (m_exception)
+		{
+			const Exception e = *m_exception;
+			m_exception.reset();
+			m_cuda_exception.reset();
+			throw e;
+		}
+
+		if (m_cuda_exception)
+		{
+			const CudaException e = *m_cuda_exception;
+			m_exception.reset();
+			m_cuda_exception.reset();
+			throw e;
+		}
+	}
+}
---
+++ RayZath/cuda_engine_renderer.cuh
@@ -0,0 +1,175 @@
+#ifndef CUDA_ENGINE_RENDERER
+#define CUDA_ENGINE_RENDERER
+
+#include "cuda_engine_parts.cuh"
+#include "cuda_world.cuh"
+
+#include <stdint.h>
+#include <thread>
+
+namespace RayZath::Cuda
+{
+	class EngineCore;
+	struct Renderer;
+
+	struct Indexer
+	{
+	private:
+		bool m_update_idx, m_render_idx;
+
+	public:
+		Indexer();
+
+	public:
+		const bool& UpdateIdx() const;
+		const bool& RenderIdx() const;
+		void Swap();
+	};
+
+	template <size_t GC>
+	struct FenceTrack
+	{
+	private:
+		std::array<RayZath::Engine::ThreadGate, GC> m_gates;
+
+
+	public:
+		FenceTrack(const bool all_opened)
+		{
+			if (all_opened)
+			{
+				for (auto& g : m_gates)
+				{
+					g.Open();
+				}
+			}
+			else
+			{
+				for (auto& g : m_gates)
+				{
+					g.Close();
+				}
+			}
+		}
+		~FenceTrack()
+		{
+			for (auto& g : m_gates)
+			{
+				g.Open();
+			}
+		}
+
+
+	public:
+		void WaitForEndOfAndClose(const size_t idx)
+		{
+			m_gates[idx].WaitAndClose();
+		}
+		void WaitForEndOf(const size_t idx)
+		{
+			m_gates[idx].Wait();
+		}
+		void CloseGate(const size_t& idx)
+		{
+			m_gates[idx].Close();
+		}
+		void OpenGate(const size_t& idx)
+		{
+			m_gates[idx].Open();
+		}
+		void OpenAll()
+		{
+			for (auto& g : m_gates)
+			{
+				g.Open();
+			}
+		}
+	};
+
+	struct TimeTable
+	{
+	private:
+		RayZath::Engine::Timer m_timer, m_cycle_timer;
+		std::vector<std::pair<std::string, float>> m_stamps;
+
+	public:
+		TimeTable();
+
+	public:
+		void AppendStage(const std::string& s);
+		void AppendFullCycle(const std::string& s);
+		void ResetTable();
+		void ResetTime();
+		std::string ToString(const uint32_t width) const;
+	};
+
+
+	struct Renderer
+	{
+		enum class State
+		{
+			None,
+			Idle,
+			Work,
+			Wait
+		};
+		enum class Stage
+		{
+			None,
+			Idle,
+			Preprocess,
+			MainRender,
+			Postprocess
+		};
+	private:
+		EngineCore* const mp_engine_core;
+
+		std::unique_ptr<std::thread> mp_render_thread;
+		std::atomic<bool> m_is_thread_alive, m_terminate_thread;
+		RayZath::Engine::ThreadGate* mp_blocking_gate;
+
+		std::unique_ptr<Exception> m_exception;
+		std::unique_ptr<CudaException> m_cuda_exception;
+
+		State m_state;
+		Stage m_stage;
+		FenceTrack<5> m_fence_track;
+
+		TimeTable m_time_table;
+
+		std::mutex m_mtx;
+
+
+	public:
+		Renderer(EngineCore* const engine_core);
+		~Renderer();
+
+
+	public:
+		void LaunchThread();
+		void TerminateThread();
+
+
+		FenceTrack<5>& GetFenceTrack();
+		const TimeTable& GetTimeTable() const;
+		const State& GetState() const;
+		const Stage& GetStage() const;
+
+	private:
+		void SetState(const State& state);
+		void SetStage(const Stage& stage);
+
+	private:
+		void RenderFunctionWrapper();
+		void RenderFunction() noexcept;
+		bool CheckTermination();
+
+		void ReportException(const Exception& e);
+		void ReportCudaException(const CudaException& e);
+		void ResetExceptions();
+	public:
+		void ThrowIfException();
+	};
+}
+
+#endif 
